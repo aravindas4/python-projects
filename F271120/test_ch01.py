18d1e6478b6df647f4fa9550f53b185c8f88dd15
Anands-88 <anandsholla8@gmail.com>
2020-11-28 22:10:40 +0530

    Add files

---
+++ F271120/test_ch01.py
@@ -0,0 +1,10 @@
+def testing():
+    print(type(5))
+    print(type(4+5.9))
+    print(type(5.0))
+    print(type(True and False))
+    first_name = "Anand"
+    last_name = 'S'
+    print(type(f"{first_name} {last_name}"))
+
+testing()
