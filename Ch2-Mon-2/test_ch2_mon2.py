6880aeebfeace3d23bd4aa3536cb90ac532261d1
Anand S <63405220+Anands-88@users.noreply.github.com>
2020-12-04 23:15:09 +0530

    Feat/ch2 mon2 (#16)
    Closes #1
    
    Name Stmts Miss Cover
    
    Ch2-Mon-2/type_check.py 2 0 100%

---
+++ Ch2-Mon-2/test_ch2_mon2.py
@@ -0,0 +1,10 @@
+from type_check import ch_one
+
+def test_ch_one():
+    assert ch_one(2) == int
+    assert ch_one(4 + 5.9) == float
+    assert ch_one(5.0) == float
+    assert ch_one(True and False) == bool
+    first_name = 'Anand'
+    last_name = 'S'
+    assert ch_one(f'{first_name} {last_name}') == str
