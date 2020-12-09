22c3a4e79761338c895f04c42a86524421606d16
Anands-88 <anandsholla8@gmail.com>
2020-12-09 22:03:59 +0530

    Modify the files

--- ch2-Tue1/test_var_output.py
+++ ch2-Tue1/test_var_output.py
@@ -3,5 +3,5 @@ from variable_output import ch2_tue1
 def test_ch_two():
     x = 3
     y = 10
-    result = x*y
+    result = x * y
     assert ch2_tue1(x,y) == result
--- ch2-Tue1/variable_output.py
+++ ch2-Tue1/variable_output.py
@@ -1,2 +1,2 @@
 def ch2_tue1(x,y):
-    return x*y
+    return x * y
