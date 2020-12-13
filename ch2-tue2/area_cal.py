70e5d213b26b15644c53c0c33ba58d3b1737844d
Aravinda S Holla <30445925+aravindas4@users.noreply.github.com>
2020-12-13 19:03:03 +0530

    Revert "Feat/ch2 tue2 (#23)"
    This reverts commit e04bb5147b620dca4f85a06dc7835ed71a80cdec.

--- ch2-tue2/area_cal.py
+++
@@ -1,4 +0,0 @@
-def add(a,b):
-    return  a * b
-   
-print(add(245.54,13.66))
--- ch2-tue2/test_area_cal.py
+++
@@ -1,8 +0,0 @@
-from area_cal import add
-
-def test_area():
-    a = 254.44
-    b = 13.66
-    area = a * b
-    assert add(a,b) == area
-
