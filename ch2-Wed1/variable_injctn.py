3760945127a3da4acb9930cfdd4dda5f0510a86c
Anands-88 <anandsholla8@gmail.com>
2020-12-17 16:56:53 +0530

    Modify the files

--- ch2-Wed1/variable_injctn.py
+++ ch2-Wed1/variable_injctn.py
@@ -1,4 +1,9 @@
-def var_inj(*var):
+def injection():
+    items = 28,20.8,True,'A'
+    var = ""
+    for value in items:
+        strng = str(value)
+        var = var + strng + ' '
     return var
 
-print(var_inj(28,20.8,True,'A'))
+print(injection())
