d1c090621ec9188fe5ed48a810477a084f0d072b
Anand S <63405220+Anands-88@users.noreply.github.com>
2020-12-19 21:38:41 +0530

    Update test_var_inj.py

--- ch2-Wed1/test_var_inj.py
+++ ch2-Wed1/test_var_inj.py
@@ -1,7 +1,7 @@
 from variable_injctn  import injection
 
 def test_inject():
-    assert injection(1, 2, 3) == "1 2 3"
-    assert injection(1, 2, 3) != "1, 2, 3"
-    assert injection('A', 8888, 'Hi there', True) == "A 8888 Hi there True"
-    assert injection(True, False) == 'True False'
+    assert injection("Any",True,6) == 'Any,True,6'
+    assert injection('December',False,19,20.20) != "December,True,19,20.20"
+    assert injection('Vi',5,'ual',5.1,'udi',0,'c',0,'de') != "Visual studio code"
+    assert injection(True, 28, 7.900) == "True,28,7.900"
