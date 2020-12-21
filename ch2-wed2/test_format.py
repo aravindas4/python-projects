d33064f34604e28ddd8c647b64d558b896d00987
Anand S <63405220+Anands-88@users.noreply.github.com>
2020-12-21 16:58:44 +0530

    Delete test_format.py

--- ch2-wed2/test_format.py
+++
@@ -1,5 +0,0 @@
-from ch2-wed2 import format_ch2
-
-def test_format():
-    assert "{}'s favorite sports is {}".format(name,sport) == "Anand's favorite sports is Cricket"
-    assert "{} is working on {} programming".format(name,program) == "Anand is working on python programming"
