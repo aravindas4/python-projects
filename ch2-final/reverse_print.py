376879d3dc0c5919e666997e7c918efcc112f70f
Anands-88 <anandsholla8@gmail.com>
2020-12-28 22:38:11 +0530

    Add new files to final issue

---
+++ ch2-final/reverse_print.py
@@ -0,0 +1,21 @@
+name = '''****************************************
+          Coding Temple, Inc.
+          283 Franklin St.
+          Boston, MA
+========================================
+    Product Name    Product Price
+    Books           $49.95
+    Computer        $579.99
+    Monitor         $124.89
+========================================
+                    Total
+                    $754.83
+========================================
+
+    Thanks for shopping with us Today
+    
+****************************************'''
+print(name)
+
+def reverse(var):
+    return var[::-1]
