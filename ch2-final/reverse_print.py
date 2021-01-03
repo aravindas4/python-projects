2cde869a54f0efd254e7c4900f67d96000cfd985
Anands-88 <anandsholla8@gmail.com>
2021-01-03 22:22:04 +0530

    Modify the files

--- ch2-final/reverse_print.py
+++ ch2-final/reverse_print.py
@@ -1,21 +1,13 @@
-name = '''****************************************
-          Coding Temple, Inc.
-          283 Franklin St.
-          Boston, MA
-========================================
-    Product Name    Product Price
-    Books           $49.95
-    Computer        $579.99
-    Monitor         $124.89
-========================================
-                    Total
-                    $754.83
-========================================
+star,equal = '*'*48,'='*48
+product = {'Books  ': 49.95,'Computer': 579.99,'Monitor':124.89}
+total = 0
+print('',star,'\n'
+    "\t\tCoding Temple, Inc.\n\t\t283 Franklin St.\n\t\tBoston, MA\n",equal,'\n\tProduct Name\tProduct Price')
+for name,price in product.items():
+    print('\t',name,'\t$',price)
+    total = total + price
 
-    Thanks for shopping with us Today
-    
-****************************************'''
-print(name)
+print('',equal,'\n\t\t\tTotal\n\t\t\t$',total,'\n',equal,'\n\n\tThanks for shopping with us today!\n\n',star)
 
 def reverse(var):
-    return var[::-1]\
+    return var[::-1]
