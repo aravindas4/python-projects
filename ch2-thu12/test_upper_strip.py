fa257b81c5f67f6ec71626f1aca2aebfe8d9ae48
Anand S <63405220+Anands-88@users.noreply.github.com>
2021-01-03 22:35:24 +0530

    Delete test_upper_strip.py

--- ch2-thu12/test_upper_strip.py
+++
@@ -1,6 +0,0 @@
-from uppercase_strip import uppercase,strip_symbols
-
-def test_function():
-    assert uppercase("LowerCase") == "LOWERCASE"
-    assert strip_symbols("&good morning&") == "good morning"
-    
