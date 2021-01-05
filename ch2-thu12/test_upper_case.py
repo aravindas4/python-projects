ed4cd7a0a4ab762502cf920f9ad0d3f9df6c5095
Anand S <63405220+Anands-88@users.noreply.github.com>
2021-01-05 22:13:32 +0530

    Delete test_upper_case.py

--- ch2-thu12/test_upper_case.py
+++
@@ -1,7 +0,0 @@
-from uppercase_strip import uppercase
-
-def test_function():
-    assert uppercase("LowerCase") == "LOWERCASE"
-    assert uppercase('UPPERCA5E') != 'UPPERCASE'
-    assert uppercase('make it uppercase') == 'MAKE IT UPPERCASE'
-    
