a0e9a841ce41b3ac4a4b2d8a178c14d4aff6bb35
Anand S <63405220+Anands-88@users.noreply.github.com>
2021-01-05 22:13:20 +0530

    Delete test_strips.py

--- ch2-thu12/test_strips.py
+++
@@ -1,7 +0,0 @@
-from uppercase_strip import strip_symbols
-
-def test_strip():
-      assert strip_symbols("&good morning&") == "good morning"
-      assert strip_symbols('^Hell0^') != 'Hello'
-      assert strip_symbols('................Dots.............') == 'Dots'
-      
