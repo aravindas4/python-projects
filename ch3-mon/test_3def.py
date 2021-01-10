64b1120c3933a84cec68861e795d89c8cd2ba505
Anand S <63405220+Anands-88@users.noreply.github.com>
2021-01-10 22:38:47 +0530

    Delete test_3def.py

--- ch3-mon/test_3def.py
+++
@@ -1,9 +0,0 @@
-from convert_input import *
-
-def test_defs():
-    assert con_bool("Hello") == bool
-    assert con_bool('') == bool
-    assert con_bool(8) == bool
-    assert sum(23,45) == 68
-    assert car_info(2018,'Purple','Nano','XMA') == '2018 Purple Nano XMA.'
-    
