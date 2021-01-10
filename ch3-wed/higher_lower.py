ee72c19032cbe71cb81ff156732ff8c7e4de9edc
Anands-88 <anandsholla8@gmail.com>
2021-01-10 22:35:28 +0530

    Add modified files

--- ch3-wed/higher_lower.py
+++ ch3-wed/higher_lower.py
@@ -3,9 +3,7 @@ def high_low(num):
         return 'Higher then 100'
     elif num < 100:
         return 'Lower then 100'
-    else:
-        return 'Equal to 100'
-
+    
 def lower(x,y):
     if x > y:
         return "lower"
--- ch3-wed/test_lower.py
+++
@@ -1,6 +0,0 @@
-from higher_lower import lower
-
-def test_low():
-    assert lower(19,28) == 'lower'
-    assert lower(98,12) == 'lower'
-    
