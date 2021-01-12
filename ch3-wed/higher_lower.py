63ca7530059d4a80406a72281dc928bd6d7bad9a
Anands-88 <anandsholla8@gmail.com>
2021-01-12 22:08:35 +0530

    Modify the files

--- ch3-wed/higher_lower.py
+++ ch3-wed/higher_lower.py
@@ -1,11 +1,11 @@
 def high_low(num):
     if num >  100:
-        return 'Higher then 100'
-    elif num < 100:
-        return 'Lower then 100'
+        return 'Higher than 100'
+    else:
+        return 'Lower than 100'
     
 def lower(x,y):
     if x > y:
-        return "lower"
-    elif x < y:
+        return "greater"
+    else:
         return "lower"
--- ch3-wed/test_highLow.py
+++ ch3-wed/test_highLow.py
@@ -1,9 +1,9 @@
 from higher_lower import *
 
 def test_highlow():
-    assert high_low(25) == 'Lower then 100'
-    assert high_low(188) == 'Higher then 100'
+    assert high_low(25) == 'Lower than 100'
+    assert high_low(188) == 'Higher than 100'
     
 def test_low():
     assert lower(19,28) == 'lower'
-    assert lower(98,12) == 'lower'
+    assert lower(78,34) != 'lower'
