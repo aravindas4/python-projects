388c8a1d11c31c2f3c08c53bbd39d6fde72e4b3c
Anand S <63405220+Anands-88@users.noreply.github.com>
2021-01-12 22:13:53 +0530

    Solution to  issue #10 (#37)
    Solution to Ch3-Thu issue #10

---
+++ ch3-wed/higher_lower.py
@@ -0,0 +1,11 @@
+def high_low(num):
+    if num >  100:
+        return 'Higher than 100'
+    else:
+        return 'Lower than 100'
+    
+def lower(x,y):
+    if x > y:
+        return "greater"
+    else:
+        return "lower"
