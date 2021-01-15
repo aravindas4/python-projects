46e1384471b654140e36cbd2b59b595fbd5d6a4f
Anands-88 <anandsholla8@gmail.com>
2021-01-15 17:00:09 +0530

    Modify the files

--- ch3-thu/error_time.py
+++ ch3-thu/error_time.py
@@ -1,13 +1,13 @@
 def fix(name):
-    if name == "Jack":
-        "Hello Jack"
+    if name != "John":
+        pass
     else:
         return "Hello John"
 
 def military_time(time):
     if time < 1200:
         return "Good Morning"
-    elif 1200 >= time <= 1700:
-        "Good Afternoon"
     elif time > 1700:
         return "Good Evening"
+    else:
+        return "Good Afternoon"
--- ch3-thu/test_issue.py
+++ ch3-thu/test_issue.py
@@ -2,9 +2,10 @@ from error_time import *
 
 def test_fix():
     assert fix("John") == "Hello John"
+    assert fix("Ajay") == None
 
 def test_military():
     assert military_time(1800) == "Good Evening"
-    assert military_time(1159) != "Good Afternoon"
+    assert military_time(1428) == "Good Afternoon"
     assert military_time(848) == "Good Morning"
     
