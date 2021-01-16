92bbd9b33e54a59e9a97226836d06fece0d3eb93
Anand S <63405220+Anands-88@users.noreply.github.com>
2021-01-16 22:04:11 +0530

    Solution to issue#11 (#38)
    Solution to issue # 11

---
+++ ch3-thu/error_time.py
@@ -0,0 +1,13 @@
+def fix(name):
+    if name != "John":
+        pass
+    else:
+        return "Hello John"
+
+def military_time(time):
+    if time < 1200:
+        return "Good Morning"
+    elif time > 1700:
+        return "Good Evening"
+    else:
+        return "Good Afternoon"
