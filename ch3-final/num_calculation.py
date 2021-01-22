0f3c8f671f492734db4642e6b5193312de15c40b
Anands-88 <anandsholla8@gmail.com>
2021-01-22 16:25:24 +0530

    Modify the files as CR raised

--- ch3-final/num_calculation.py
+++ ch3-final/num_calculation.py
@@ -1,13 +1,13 @@
-
 def calculator(operator,num1,num2,reverse): 
-    if reverse == 'r' and operator in ('add','+'): 
-        return float(num2 + num1) 
-    elif reverse == 'r' and operator in ('sub','-'): 
-        return float(num2 - num1) 
-    elif reverse == 'r' and operator in ("mul",'*'): 
-        return float(num2 * num1) 
-    elif reverse == 'r' and operator in('div','/'): 
-       return float(num2 / num1) 
+    if reverse == True:
+        if operator in ('add','+'): 
+            return float(num2 + num1) 
+        elif operator in ('sub','-'): 
+            return float(num2 - num1) 
+        elif operator in ("mul",'*'): 
+            return float(num2 * num1) 
+        elif operator in('div','/'): 
+            return float(num2 / num1) 
 
     if operator in ('add','+'): 
       return float(num1 + num2) 
@@ -17,6 +17,4 @@ def calculator(operator,num1,num2,reverse):
         return float(num1 * num2) 
     elif operator in ('div','/'): 
         return float(num1 / num2) 
-    
    
-print(calculator("add",6,48,None))
